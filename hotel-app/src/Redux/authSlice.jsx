89941a5763ef7774b766f1a2d6ae092a6c3d1744
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-04 10:41:31 +0200

    Register through redux-firease works

---
+++ hotel-app/src/Redux/authSlice.jsx
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../Firebase/firebase";
+
+// Async action to handle user register
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userCredential.user;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    error: null,
+    loading: false,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
---
+++ hotel-app/src/Redux/store.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./authSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: userReducer,
+  },
+});
+
+export default store;
